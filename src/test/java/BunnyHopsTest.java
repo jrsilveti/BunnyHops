b4a4788aa6c60ba662fa5240e6e94fa9f0750439
jrsilveti <josersilveti27@gmail.com>
2022-06-04 10:54:19 -0400

    Initial project setup

---
+++ src/test/java/BunnyHopsTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BunnyHopsTest {
+
+    BunnyHops bunnyHops = new BunnyHops();
+
+
+    @Test
+    public void zeroSteps() {
+        int result = bunnyHops.bunnyHops(0);
+
+        Assertions.assertEquals(0, result);
+    }
+
+    @Test
+    public void fourSteps() {
+        int result = bunnyHops.bunnyHops(4);
+
+        Assertions.assertEquals(7, result);
+    }
+}
